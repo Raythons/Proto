7dd690bcaf1ee63074f0c61a6a889afcdedfaa40
Rasheed Asfar <rasheed998855@gmail.com>
2024-09-26 04:34:20 +0300

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -66,7 +66,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="www.linkedin.com/in/rasheed-asfar-4385b7267"
+                  href="https://linkedin.com/in/rasheed-asfar-4385b7267/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
